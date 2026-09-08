481044808da539b212c2b8c3354d955b155071ef
Simone Dall'Olio <simo.dallolio@gmail.com>
2026-09-08 12:56:09 +0200

    Prefer fresh PWA assets over cache

--- pwa/sw.js
+++ pwa/sw.js
@@ -13,5 +13,13 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
