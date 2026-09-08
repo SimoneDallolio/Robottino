c8a7487689b18ae96b883770a163c6684006a6ac
Simone Dall'Olio <simo.dallolio@gmail.com>
2026-09-08 12:26:14 +0200

    Refresh PWA service worker cache

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'robottino-ble-v1';
+const CACHE = 'robottino-ble-v2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
